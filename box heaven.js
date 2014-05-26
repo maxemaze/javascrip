3374a3c18288d3063bfba4f4d72deab632d9ed16
maxemaze <tentimeseight@gmail.com>
2014-05-25 22:20:15 -0400

    box heave n js

---
+++ box heaven.js
@@ -0,0 +1,30 @@
+	var count = 0;
+	var r, g, b;
+	var canvas, ctx;
+	var timing;
+	
+	var wid = window.innerWidth;
+	
+	function setup() {
+		canvas = document.createElement('canvas');
+		canvas.height = window.innerHeight; canvas.width = wid;
+		document.body.appendChild(canvas);
+		ctx = canvas.getContext('2d');
+		
+		timing = setInterval(drawMyRect, 40);
+	}
+	
+	function drawMyRect() { 
+		r = Math.floor(Math.random()*256);
+		g = Math.floor(Math.random()*256);
+		b = Math.floor(Math.random()*256);
+		
+		ctx.fillStyle = "rgb("+r+","+g+","+b+")";
+		ctx.fillRect((count)*200, 100, Math.floor(Math.random()*100)+50, 100);
+		
+		count++;
+		count %= (wid/200);
+	}
+		
+	
+	setup();
